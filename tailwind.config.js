519e230f8d4f0b0a3c6a6905fa9f70554e67fc1e
Masanaser <masadawood29@gmail.com>
2025-01-03 22:15:24 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Inter:['Inter','sans-serif']
+    }, 
+       
+    extend: {
+      screens:{
+        '2md':'944px',
+        's':'425px',
+        's':'326px'
+      },
+      colors:{
+       primary: '#7E3AF2',
+       secondaryText: '#6B7280',
+       mainText: '#111928',
+       BorderColor:'#E5E7EB'
+    },
+  },
+  },
+  plugins: [],
+}
+
